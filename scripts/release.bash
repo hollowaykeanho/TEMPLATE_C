095794a4219eaf3bba0cccff7bb43c1fc03eaa9c
(Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>
2018-02-14 12:17:06 +0800

    scripts: added auto fetch from origin before generating changes
    The existing release script does not fetch updates from the origin.
    Hence, this makes the change generation detected as nothing has
    changed from the release machine.
    
    This patch adds git fetch commands for both master and staging
    branches before performing the updates, making the changes visible
    in the local release machine.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <kean.ho.chew@zoralab.com>

--- scripts/release.bash
+++ scripts/release.bash
@@ -9,6 +9,8 @@ release() {
 
 	reference="origin/master"
 	current="origin/feza/staging"
+	git fetch origin master:master
+	git fetch origin feza/staging:feza/staging
 	diff=$(git log --oneline "$reference..$current")
 	timestamp=$(date '+%A %T - %B/%d/%Y (%z)')
 	newlog="\
